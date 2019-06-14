2e414f4d9516e05e98e8e6aa5e4b1732ece27d50
adved85 <adved85@gmail.com>
2019-06-14 12:59:55 +0400

    solving bug - logout or expired on one page, the other one now redirect to index instead of error show

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -26,9 +26,13 @@ class CheckRole
         }
 
         /* User-STATUS --- does he active or not */
-        if ( $request->user()->status !== 1) {
+        if ($request->user() && $request->user()->status !== 1) {
             Auth::logout();
             return redirect()->route('login', app()->getLocale())->with('blocked_msg', trans('auth.blocked')); // 'You are blocked flash-alert
+        }else{
+            // եթե մի էջի վրա լոգաութա եղել, մյուսի վրա , եթե էլի login ուղարկեինք, ERROR կտար, քանի որ այս կայքում մենյուն դատայա ստանում մեթոդից,
+            // որը այս պարագայում շատ֊շատ return view('auth.login', ['data' =>$data]) սենց մի բանով լուծվեր։
+            return redirect()->route('index_page', app()->getLocale());
         }
 
 
